0b35159cae7565ba746684162b74e53b332763ec
Ehsan-0831 <kohansalc9@gmail.com>
2016-12-15 12:20:37 +0800

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '261040629:AAGxs-AWaxfzl2AxTueTCQ9OAaQfncAMxCU');
+define('BOT_TOKEN', '256510272:AAGWC66N_3mDgjYksONTeStXZXGHlXySJt8');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
